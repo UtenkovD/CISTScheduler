fc9d75c829f01d9bceac6866a6f59e23096908f0
Dmitry Utenkov <dmitry.utenkov@coderivium.com>
2013-06-14 02:06:58 +0300

    Initial files uploading

---
+++ CISTScheduler/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  CISTScheduler
+//
+//  Created by Dmitry Utenkov on 6/14/13.
+//  Copyright (c) 2013 Coderivium. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ CISTScheduler/ScheduleViewController.h
@@ -0,0 +1,13 @@
+//
+//  ScheduleViewController.h
+//  CISTScheduler
+//
+//  Created by Dmitry Utenkov on 6/14/13.
+//  Copyright (c) 2013 Coderivium. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ScheduleViewController : UITableViewController
+
+@end
