1e47bcb6d6cc1c1bde0455afa426af42d5701fab
Dmitry Utenkov <dmitry.utenkov@coderivium.com>
2013-06-16 02:40:49 +0300

    Added key files for groups

---
+++ CISTScheduler/GroupPickerDataSource.h
@@ -0,0 +1,13 @@
+//
+//  GroupPickerDataSource.h
+//  CISTScheduler
+//
+//  Created by Dmitry Utenkov on 6/16/13.
+//  Copyright (c) 2013 Coderivium. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface GroupPickerDataSource : NSObject
+
+@end
---
+++ CISTScheduler/GroupPickerDataSource.m
@@ -0,0 +1,13 @@
+//
+//  GroupPickerDataSource.m
+//  CISTScheduler
+//
+//  Created by Dmitry Utenkov on 6/16/13.
+//  Copyright (c) 2013 Coderivium. All rights reserved.
+//
+
+#import "GroupPickerDataSource.h"
+
+@implementation GroupPickerDataSource
+
+@end
